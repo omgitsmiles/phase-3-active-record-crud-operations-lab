7dc13dd656edc93139e6f4694adb60e948e628d9
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-14 22:44:46 -0400

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,50 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title(title)
+            self.create(title: title)
+            # creates and saves in one
+     end
+
+     def self.first_movie
+        self.first
+     end
+
+     def self.last_movie
+        self.last
+     end
+     
+     def self.movie_count
+        self.count
+     end
+
+     def self.find_movie_with_id(id)
+        self.find(id)
+     end
+
+     def self.find_movie_with_attributes(attributes)
+        self.find_by(attributes)
+     end
+
+     def self.find_movies_after_2002
+        self.where("release_date > ?", 2002)
+     end
+
+     def update_with_attributes(attributes)
+        self.update(attributes)
+     end
+
+     def self.update_all_titles(title)
+        self.update(title: title)
+        # passes the string in method
+        # title: targets title specifically with title passed in
+     end
+
+     def self.delete_by_id(id)
+        self.destroy(id)
+     end
+
+     def self.delete_all_movies
+        self.destroy_all
+     end
+
 end
