6408749b51762054915a91b144774cc8bc5a7064
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-11-30 14:27:29 -0500

    commit tree 2b76c03b9f8b12b764249ecec26207211867a403

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,50 +1,106 @@
+# class Movie < ActiveRecord::Base
+
+# @@movie = []
+# # has access to all class scopes
+
+
+#       def self.create_with_title(title)
+#             self.create(title: title)
+#             # creates and saves in one
+#       end
+
+#      def self.first_movie
+#       # instance variable only acces w instances
+#       # class varaibles only 
+#       # even tho inherits from AR it can still use initialize and other methods in class
+#         self.first
+#      end
+
+#      def self.last_movie
+#         self.last
+#      end
+     
+#      def self.movie_count
+#         self.count
+#      end
+
+#      def self.find_movie_with_id(id)
+#         self.find(id)
+#      end
+
+#      def self.find_movie_with_attributes(attributes)
+#         self.find_by(attributes)
+#      end
+
+#      def self.find_movies_after_2002
+#         self.where("release_date > ?", 2002)
+#      end
+
+#      def update_with_attributes(attributes)
+#         self.update(attributes)
+#      end
+
+#      def self.update_all_titles(title)
+#         self.update(title: title)
+#         # passes the string in method
+#         # title: targets title specifically with title passed in
+#      end
+
+#      def self.delete_by_id(id)
+#         self.destroy(id)
+#      end
+
+#      def self.delete_all_movies
+#         self.destroy_all
+#      end
+
+# end
+
+
 class Movie < ActiveRecord::Base
 
-    def self.create_with_title(title)
-            self.create(title: title)
-            # creates and saves in one
-     end
+   def self.create_with_title(title)
+      self.create(title: title)
+   end
 
-     def self.first_movie
-        self.first
-     end
+   def self.first_movie
+      self.first
+   end
 
-     def self.last_movie
-        self.last
-     end
-     
-     def self.movie_count
-        self.count
-     end
+   def self.last_movie
+      self.last
+   end
 
-     def self.find_movie_with_id(id)
-        self.find(id)
-     end
+   def self.movie_count
+      self.count
+   end
 
-     def self.find_movie_with_attributes(attributes)
-        self.find_by(attributes)
-     end
+   def self.find_movie_with_id(num)
+      self.find(num)
+   end
 
-     def self.find_movies_after_2002
-        self.where("release_date > ?", 2002)
-     end
+   def self.find_movie_with_attributes(attr)
+      self.find_by(attr)
+   end
 
-     def update_with_attributes(attributes)
-        self.update(attributes)
-     end
+   def self.find_movies_after_2002
+      self.where("release_date > ?", 2002)
+   end
 
-     def self.update_all_titles(title)
-        self.update(title: title)
-        # passes the string in method
-        # title: targets title specifically with title passed in
-     end
+   def update_with_attributes(attr)
+      self.update(attr)
+   end
 
-     def self.delete_by_id(id)
-        self.destroy(id)
-     end
+   def self.update_all_titles(title)
+      self.update(title: title)
+   end
 
-     def self.delete_all_movies
-        self.destroy_all
-     end
+   def self.delete_by_id(id)
+      self.destroy(id)
+   end
+
+   def self.delete_all_movies
+      self.destroy_all
+   end
 
 end
